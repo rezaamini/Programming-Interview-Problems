ae3a11f4390e4d39aa8a79998124e3cbba87b645
rezaamini <reza.amini@yahoo.com>
2014-12-18 13:55:10 -0500

    Update RemoveLinkedListDuplicates.java

--- LinkedLists/RemoveLinkedListDuplicates.java
+++ LinkedLists/RemoveLinkedListDuplicates.java
@@ -1,12 +1,14 @@
 /*
+
  Imagine a linked list that has duplicate items. Write an algorithm to remove 
  duplicates without using any extra space or changing the data structure.
- */
+
+*/
 package LinkedLists;
 
 /**
  *
- * @author rezaamini
+ * @author Reza Amini
  */
 public class RemoveLinkedListDuplicates<T>
 {
@@ -26,11 +28,36 @@ public class RemoveLinkedListDuplicates<T>
             while(runner != null && runner.next != null)
             {
                 if(current.value.equals(runner.next.value))
-                    runner.next = runner.next.next;
-                runner = runner.next;
+                    runner.next = runner.next.next; // this line will take the runner one step forward itself
+                else
+                    runner = runner.next;
             }
             current = current.next;
         }
         return head;
+    }
+}
+
+class LinkedListNode<T>
+{
+    T value;
+    LinkedListNode<T> next;
+    
+    public LinkedListNode(T value)
+    {
+        this.value = value;
+    }
+    
+    public String toString()
+    {
+        StringBuilder out = new StringBuilder();
+        LinkedListNode<T> temp = this;
+        while(temp != null)
+        {
+            out.append(temp.value).append(" ");
+            temp = temp.next;
+        }
+        out.setLength(out.length() - 1);
+        return out.toString();
     }
 }
