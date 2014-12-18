bdc7abee3814eafa3945cbcb0eb558ee796a28e7
rezaamini <reza.amini@yahoo.com>
2014-12-18 13:25:50 -0500

    Update FindMiddle.java

--- LinkedLists/FindMiddle.java
+++ LinkedLists/FindMiddle.java
@@ -1,15 +1,22 @@
 /*
- Write an algorithm to find the middle of a linked list. If the list is 
- even-sized, the middle is the node that is closer to the beginning.
- */
+
+  Write an algorithm to find the middle of a linked list. If the list is 
+  even-sized, the middle is the node that is closer to the beginning.
+ 
+*/
+
 package LinkedLists;
 
 /**
  *
- * @author rezaamini
+ * @author Reza Amini
  */
 public class FindMiddle<T>
 {
+    // Use two pointers: a fast and a slow pointer. The fast pointer's speed is twice the slow one.
+    // When the fast pointer reaches the end on the linkedlist, the slow one is in the middle.
+    // Time complexity: O(N)
+    // Space complexity: O(1)
     public LinkedListNode<T> findMiddle(LinkedListNode<T> head)
     {
         if(head == null)
