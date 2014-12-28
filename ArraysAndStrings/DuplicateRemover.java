05b522ce017a899a78acd1244a96a60cf5a72b3e
rezaamini <reza.amini@yahoo.com>
2014-12-27 22:50:04 -0500

    Update DuplicateRemover.java

--- ArraysAndStrings/DuplicateRemover.java
+++ ArraysAndStrings/DuplicateRemover.java
@@ -18,8 +18,11 @@ public class DuplicateRemover
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-        char[] input = scan.nextLine().toCharArray();
-        System.out.println(removeDuplicates(input));
+        String input = scan..nextLine();
+        char[] chars = input.toCharArray();
+        System.out.println(removeDuplicates(chars));
+        System.out.println(removeDuplicates(new StringBuilder(input));
+        System.out.println(removeDuplicates(input);
     }
     
     // Time complexity: O(N)
@@ -44,5 +47,45 @@ public class DuplicateRemover
         for(int i = 0; i < index; i++)
             out.append(input[i]);
         return out.toString();
+    }
+    
+    /* 
+     * Without additional buffer.
+     * We get the input as a StringBuilder and remove the duplicates in place.
+     * Use StringBuilder when you want to concatinate strings in loops to use less space
+     * Time complexity: O(n^2)
+     */
+    public static String removeDuplicates(StringBuilder str) 
+    {
+        int index = 1; // index of the next unique character in output
+        for (int i = 1 ; i < str.length() ; i++) // for all characters in the string
+        {
+            int j; // runner pointer for previous unique characters
+            for (j = 0 ; j < index ; j++) // compare the characters with previous unique ones
+            { 
+                if (str.charAt(i) == str.charAt(j))
+                    break;
+            }
+
+            if (j == index) 
+            {
+                str.setCharAt(index, str.charAt(i));
+                index++;
+            }
+        }
+
+        str.setLength(index);
+        return new String(str);
+    }
+
+    // Time complexity: O(n^2)
+    // Space complexity: O(n)
+    public static String removeDuplicates(String str) {
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i < str.length(); i++) {
+            if (output.indexOf(str.substring(i, i+1)) == -1)
+                output.append(str.charAt(i));
+        }
+        return output.toString();
     }
 }
