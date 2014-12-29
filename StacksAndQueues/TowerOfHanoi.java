844a73cdd448b293a4bb9c17d6bf78b23ff26523
rezaamini <reza.amini@yahoo.com>
2014-12-28 23:48:55 -0500

    Update TowerOfHanoi.java

--- StacksAndQueues/TowerOfHanoi.java
+++ StacksAndQueues/TowerOfHanoi.java
@@ -1,9 +1,14 @@
 /*
 
-   Implement tower of Hanoi problem.
-   # of towers = 3
-   # of disks in each tower = 5
-
+   In the classic problem of the Towers of Hanoi, you have 3 rods and N disks
+   of different sizes which can slide onto any tower. The puzzle starts with
+   disks sorted in ascending order of size from top to bottom (e.g., each disk
+   sits on top of an larger one). You have the following constraints:
+   (A) Only one disk can be moved at a time.
+   (B) A disk is slid off the top of one rod onto the next rod.
+   (C) A disk can only be placed on top of a larger disk.
+   Write a program to move the disks from the first rod to the last using Stacks.
+ 
 */
 package Stacks_And_Queues;
 
@@ -59,6 +64,7 @@ class Tower
     
     // Moves n disks to the destination tower utilizing a tower as buffer
     // Use recursion
+    // Time complexity: O(n)
     public void moveTo(int n, Tower des, Tower buf)
     {
         if (n > 0)
